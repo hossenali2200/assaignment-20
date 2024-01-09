3737662e342cfee79561510cec65c8056124e903
hossenali2200 <hosenali2200@gmail.com>
2024-01-09 15:13:48 +0600

    Building a Sales Analytics API
     Building a Sales Analytics API

---
+++ sales-analytics-api/Sale.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const saleSchema = new mongoose.Schema({
+  product: String,
+  quantity: Number,
+  price: Number,
+  date: Date
+});
+
+module.exports = mongoose.model('Sale', saleSchema);
